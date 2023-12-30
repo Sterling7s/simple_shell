870b1e6f2f07378174ddd3e4bea664db93988224
Sterling7s <ibrahimabubakarsiddiq4@gmail.com>
2023-12-30 21:47:12 +0000

    This programme passes the betty test

---
+++ 0_betty_would_be_proud.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - This code passes the betty checks
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	m = 34;
+	n = 23;
+	printf("This is the sum of the two letters:%d/n", m + n);
+	return (0);
+}
